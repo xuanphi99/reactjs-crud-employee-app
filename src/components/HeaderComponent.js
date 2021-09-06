bc71bb3e5da886f96fb85f48370e7a831157460f
xuanphi <doanxuanphi2@gmail.com>
2021-09-07 02:48:49 +0700

    change titile

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -13,7 +13,7 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="https://xuanphi99.github.io/" className="navbar-brand">Employee Management App</a></div>
+                    <div><a href="https://xuanphi99.github.io/" className="navbar-brand">CRUD Nhân Viên App</a></div>
                     </nav>
                 </header>
             </div>
